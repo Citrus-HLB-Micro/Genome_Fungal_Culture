4e2d4fb7e3f29f5f3218435c9ee02dc183efff45
tania-k <tkurb001@ucr.edu>
2022-04-12 16:52:44 -0700

    update to skip pilon dont have that yet for most of thee

--- assembly/pipeline/stats/01_asm_stats.sh
+++ assembly/pipeline/stats/01_asm_stats.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -l
-#SBATCH -p short -N 1 -n 2 --mem 4gb --out logs/assess.log
+#SBATCH -p short -N 1 -n 2 --mem 4gb --out logs/stats.log
 
 module load AAFTF
 
@@ -9,7 +9,7 @@ INDIR=asm
 OUTDIR=genomes
 
 mkdir -p $OUTDIR
-while read STRAIN NANOPORE ILLUMINA LOCUS
+while read STRAIN NANOPORE
 do
     rsync -a $INDIR/canu/$STRAIN/$STRAIN.contigs.fasta $OUTDIR/$STRAIN.canu.fasta
     rsync -a $INDIR/flye/$STRAIN/assembly.fasta $OUTDIR/$STRAIN.flye.fasta
@@ -24,7 +24,8 @@ do
 		AAFTF assess -i $OUTDIR/$STRAIN.$type.fasta -r $OUTDIR/$STRAIN.$type.stats.txt
 	    fi
 	fi
-	for polishtype in medaka pilon
+	#for polishtype in medaka pilon
+	for polishtype in medaka 
 	do
 	    if [[ -s $OUTDIR/$STRAIN.${type}.$polishtype.fasta ]]; then
 		if [[ ! -f $OUTDIR/$STRAIN.$type.$polishtype.stats.txt || $OUTDIR/$STRAIN.$type.$polishtype.fasta -nt $OUTDIR/$STRAIN.$type.$polishtype.stats.txt ]]; then
@@ -36,12 +37,13 @@ do
     if [[ ! -f $OUTDIR/$STRAIN.necat.stats.txt || $OUTDIR/$STRAIN.necat.fasta -nt $OUTDIR/$STRAIN.necat.stats.txt ]]; then
     	AAFTF assess -i $OUTDIR/$STRAIN.necat.fasta -r $OUTDIR/$STRAIN.necat.stats.txt
     fi
-    type=necat
-    polishtype=pilon
-    rsync -a $INDIR/pilon/$STRAIN/$type.$polishtype.fasta $OUTDIR/$STRAIN.$type.$polishtype.fasta
-    if [[ ! -f $OUTDIR/$STRAIN.$type.$polishtype.stats.txt || $OUTDIR/$STRAIN.$type.$polishtype.fasta -nt $OUTDIR/$STRAIN.$type.$polishtype.stats.txt ]]; then
-	    AAFTF assess -i $OUTDIR/$STRAIN.$type.$polishtype.fasta -r $OUTDIR/$STRAIN.$type.$polishtype.stats.txt
-    fi
+
+    #type=necat
+    #polishtype=pilon
+    #rsync -a $INDIR/pilon/$STRAIN/$type.$polishtype.fasta $OUTDIR/$STRAIN.$type.$polishtype.fasta
+    #if [[ ! -f $OUTDIR/$STRAIN.$type.$polishtype.stats.txt || $OUTDIR/$STRAIN.$type.$polishtype.fasta -nt $OUTDIR/$STRAIN.$type.$polishtype.stats.txt ]]; then
+	#    AAFTF assess -i $OUTDIR/$STRAIN.$type.$polishtype.fasta -r $OUTDIR/$STRAIN.$type.$polishtype.stats.txt
+    #fi
 
 done < $SAMPLES
 
