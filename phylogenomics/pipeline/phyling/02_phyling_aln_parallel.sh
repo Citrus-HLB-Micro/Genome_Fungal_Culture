ec9b4b0ce298071d5f6fc8bd4fd415f35f5f2a68
tania-k <tkurb001@ucr.edu>
2022-04-17 07:31:40 -0700

    setup for parallel run

--- phylogenomics/pipeline/phyling/02_phyling_aln_parallel.sh
+++ phylogenomics/pipeline/phyling/02_phyling_aln_parallel.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash -l
-#SBATCH --ntasks 32 --mem 24G --time 2:00:00 -p short -N 1
+#SBATCH --ntasks 96 --mem 96G --time 2:00:00 -p short -N 1 --out logs/align_parallel.log
+
 module load miniconda3
 conda activate /bigdata/stajichlab/shared/condaenv/phyling
+#module load parallel -- installed in the conda env now
+#module load biopython -- installed in the conda env now
+#module load hmmer/3 --installed in the conda env
 
 if [ ! -f config.txt ]; then
     echo "Need config.txt for PHYling"
