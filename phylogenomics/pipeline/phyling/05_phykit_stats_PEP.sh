a500d77364079eec2918aa2a328527708925602d
tania-k <tkurb001@ucr.edu>
2022-04-17 13:57:54 -0700

    fixed sumstats

--- phylogenomics/pipeline/phyling/05_phykit_stats_PEP.sh
+++ phylogenomics/pipeline/phyling/05_phykit_stats_PEP.sh
@@ -11,19 +11,21 @@ PEPTREEEXT=aa.clipkit.FT.tre
 mkdir -p gene_trees
 pushd gene_trees
 ln -s ../aln/$HMM/*.${PEPTREEEXT} .
+ln -s ../aln/$HMM/*.$(basename $PEPTREEEXT .FT.tre) .
+find -L . -size 0 | xargs rm
 
 module load phykit
 module load parallel
 summarize() {
-	treefile = $1
-	aln      = $(basename $1 .FT.tre)
-	len      = $(phykit aln_len $aln)
-	bss      = $(phykit bss $aln)
-	meanBSS   = $(echo $bss | grep mean: | awk '{print $2}')
-	medianBSS = $(echo $bss | grep median: | awk '{print $2}')
-
-	echo -e "$treefile\t$aln\t$len\t$meanBSS\t$medianBSS\t"
+treefile=$(basename $1)
+aln=$(basename $treefile .FT.tre)
+len=$(phykit aln_len $aln)
+bss=$(phykit bss $treefile)
+evorate=$(phykit evolutionary_rate $treefile)
+meanBSS=$(echo $bss | grep mean: | awk '{print $2}')
+medianBSS=$(echo $bss | grep median: | awk '{print $2}')
+echo -e "$treefile\t$aln\t$len\t$meanBSS\t$medianBSS\t$evorate"
 }
 export -f summarize
-echo -e "TREE\tALN\tALNLEN\tmean_BSS\tmedian_BSS" > ../gene_trees.summarize_PEP.tsv
-parallel -j $CPU summarize ::: $(ls *.${PEPTREEEXT}) >> ../gene_trees.summarize_PEP.tsv
+echo -e "TREE\tALN\tALNLEN\tmean_BSS\tmedian_BSS\tevoRate" > ../gene_trees.summarize_PEP.tsv
+parallel -j $CPU summarize ::: $(find . -name "*.${PEPTREEEXT}" ) >> ../gene_trees.summarize_PEP.tsv
