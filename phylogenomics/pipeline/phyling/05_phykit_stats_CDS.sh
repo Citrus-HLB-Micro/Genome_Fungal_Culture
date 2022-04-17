b087d686673e7ebd79b15395145db74c4ac38e8b
tania-k <tkurb001@ucr.edu>
2022-04-17 11:29:32 -0700

    summarize tree stats

---
+++ phylogenomics/pipeline/phyling/05_phykit_stats_CDS.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+#SBATCH -p short -N 1 -n 32 --mem 64gb --out phykit_summarize.log
+
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+  CPU=$SLURM_CPUS_ON_NODE
+fi
+
+source config.txt
+PEPTREEEXT=cds.clipkit.FT.tre
+mkdir -p gene_trees
+pushd gene_trees
+ln -s ../aln/$HMM/*.${PEPTREEEXT} .
+
+module load phykit
+module load parallel
+summarize() {
+	treefile = $1
+	aln      = $(basename $1 .FT.tre)
+	len      = $(phykit aln_len $aln)
+	bss      = $(phykit bss $aln)
+	meanBSS   = $(echo $bss | grep mean: | awk '{print $2}')
+	medianBSS = $(echo $bss | grep median: | awk '{print $2}')
+
+	echo -e "$treefile\t$aln\t$len\t$meanBSS\t$medianBSS\t"
+}
+export -f summarize
+echo -e "TREE\tALN\tALNLEN\tmean_BSS\tmedian_BSS" > ../gene_trees.summarize_CDS.tsv
+parallel -j $CPU summarize ::: $(ls *.${PEPTREEEXT}) >> ../gene_trees.summarize_CDS.tsv
