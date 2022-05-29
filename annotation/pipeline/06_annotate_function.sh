5a58b550a3592a95638e479829be4abb17b120b3
tania-k <tkurb001@ucr.edu>
2022-05-29 12:20:06 -0700

    source local login bashrc and only need to unload miniconda3

--- annotation/pipeline/06_annotate_function.sh
+++ annotation/pipeline/06_annotate_function.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -l
 #SBATCH --nodes=1
 #SBATCH --ntasks=16 --mem 16gb
 #SBATCH --output=logs/annotfunc.%a.log
 #SBATCH --time=2-0:00:00
 #SBATCH -p intel -J annotfunc
 
-module unload miniconda2 miniconda3 perl python
+module unload miniconda3
 module load funannotate
 module load phobius
 
